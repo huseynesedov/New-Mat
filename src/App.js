67bebec9d4a652878f87ed51de84f082f93b5a51
murad-narimanli <murad.narimanli@mail.ru>
2024-11-14 15:54:06 +0400

    changes for bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { useEffect } from 'react';
 import RouteList from './Components/Layout/Routes/Routes';
 import Layout from './Components/Layout/MainLayout/Layout';
 import Login from './Components/Pages/Login/Login';
@@ -13,22 +13,50 @@ function App() {
     return <SkeletonScreen />;
   }
 
-  setInterval(()=>{
-    if(loggedIn) {
-      return AccountApi.RefreshToken({
-        refreshToken:localStorage.getItem('refreshToken')
-      }).then((response)=>{
-        console.log(response);
-        localStorage.setItem('refreshToken', response.refreshToken);
-        localStorage.setItem('token', response.accessToken);
-      }).catch(()=>{
-        logout()
-      })
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(()=>{
+    updateToken()
+  }, [])
+
+  const decodeJwt = (token) => {
+    try {
+      const payloadBase64 = token.split(".")[1]; // Token-in ikinci hissəsi payload hissəsidir
+      const decodedPayload = atob(payloadBase64); // Base64 formatından decode edirik
+      return JSON.parse(decodedPayload); // JSON formatına çeviririk
+    } catch (error) {
+      console.error("Token decoding error:", error); // Hata varsa konsolda göstəririk
+      return null;
     }
-    else{
-      return;
-    }
-  }, 30000)
+  };
+
+
+  const updateToken = () => {
+    let t = localStorage.getItem('token')
+    let dec = decodeJwt(t)
+    let timeout = (dec.exp - dec.iat - 200) * 1000
+
+    setInterval(()=>{
+      if(loggedIn) {
+        return AccountApi.RefreshToken({
+          refreshToken:localStorage.getItem('refreshToken')
+        }).then((response)=>{
+          console.log(response);
+          localStorage.setItem('refreshToken', response.refreshToken);
+          localStorage.setItem('token', response.accessToken);
+        }).catch(()=>{
+          logout()
+        })
+      }
+      else{
+        return;
+      }
+    }, timeout)
+  }
+
+
+
+
 
   return (
     <>
